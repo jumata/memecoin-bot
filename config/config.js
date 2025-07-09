29e32a6bee18ac825799decb167150ab5a562425
Jumata96 <54186761+Jumata96@users.noreply.github.com>
2025-07-09 16:55:36 -0500

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+module.exports = {
+  BIRDEYE_API_KEY: process.env.BIRDEYE_API_KEY,
+  BASE_URL: 'https://api.birdeye.so/v1',
+  BINANCE_API_KEY: process.env.BINANCE_API_KEY,
+  // Otras configuraciones
+};
